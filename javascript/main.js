45d17af9bfcb35d9e52c8a0d38093a627d323ac0
JP <parker.tech9@gmail.com>
2019-03-29 02:55:40 -0400

    set up the header with bootstrap and the link/scripts needed for all utilities

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBVOKtkxCVlgKNpZpu9kPUztr6JUJEXXqk",
+  authDomain: "rps-project-online.firebaseapp.com",
+  databaseURL: "https://rps-project-online.firebaseio.com",
+  projectId: "rps-project-online",
+  storageBucket: "rps-project-online.appspot.com",
+  messagingSenderId: "14469084525"
+};
+firebase.initializeApp(config);
